5a54141f855733a70769a7bf7eae1f1c3daf6e8e
James Smith <jasmith.188@gmail.com>
2021-04-15 21:16:06 -0400

    change some media queries

--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -20,52 +20,53 @@ import { Carousel } from 'react-responsive-carousel';
 const OurStory = () => {
   return (
     <div className="ourStory" id="ourStory">
-      <Carousel>
-        <div>
-          <img src={Image1} alt="katieandjimmypic" />
-        </div>
-        <div>
-          <img src={Image2} alt="katieandjimmypic" />
-        </div>
-
-        <div>
-          <img src={Image4} alt="katieandjimmypic" />
-        </div>
-
-        <div>
-          <img src={Image6} alt="katieandjimmypic" />
-        </div>
-        <div>
-          <img src={Image7} alt="katieandjimmypic" />
-        </div>
-        <div>
-          <img src={Image8} alt="katieandjimmypic" />
-        </div>
-        <div>
-          <img src={Image9} alt="katieandjimmypic" />
-        </div>
-        <div>
-          <img src={Image10} alt="katieandjimmypic" />
-        </div>
-        <div>
-          <img src={Image11} alt="katieandjimmypic" />
-        </div>
-        <div>
-          <img src={Image12} alt="katieandjimmypic" />
-        </div>
-        <div>
-          <img src={Image13} alt="katieandjimmypic" />
-        </div>
-        <div>
-          <img src={Image14} alt="katieandjimmypic" />
-        </div>
-      </Carousel>
       <h1>Our Story</h1>
       <p>
         Jimmy and Katie met in 2013 while working aboard the Pride of America in
         Hawai'i. Their first 'official' date was snorkeling at Hanuma Bay on the
         big island of O'ahu - it was forever after that.
       </p>
+      <Carousel className="ourStory__carousel">
+        <div className='ourStory__carousel-slider'>
+          <img className='ourStory__image' src={Image1} alt="katieandjimmypic" />
+        </div>
+        <div>
+          <img className='ourStory__image' src={Image2} alt="katieandjimmypic" />
+        </div>
+
+        <div>
+          <img className='ourStory__image' src={Image4} alt="katieandjimmypic" />
+        </div>
+
+        <div>
+          <img className='ourStory__image' src={Image6} alt="katieandjimmypic" />
+        </div>
+        <div>
+          <img className='ourStory__image' src={Image7} alt="katieandjimmypic" />
+        </div>
+        <div>
+          <img className='ourStory__image' src={Image8} alt="katieandjimmypic" />
+        </div>
+        <div>
+          <img className='ourStory__image' src={Image9} alt="katieandjimmypic" />
+        </div>
+        <div>
+          <img className='ourStory__image' src={Image10} alt="katieandjimmypic" />
+        </div>
+        <div>
+          <img className='ourStory__image' src={Image11} alt="katieandjimmypic" />
+        </div>
+        <div>
+          <img className='ourStory__image' src={Image12} alt="katieandjimmypic" />
+        </div>
+        <div>
+          <img className='ourStory__image' src={Image13} alt="katieandjimmypic" />
+        </div>
+        <div>
+          <img className='ourStory__image' src={Image14} alt="katieandjimmypic" />
+        </div>
+      </Carousel>
+      
     </div>
   );
 };
--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -8,76 +8,58 @@ const RSVP = () => {
     return <p>Thanks for your RSVP! We can't wait to see you there!</p>;
   }
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>Name of Guest(s): </label>
-        <input type="text" name="Guest(s) Name" required />
-        <br />
-        <label>Number of Guest(s): </label>
-        <label type="number" name="number" required></label>
-        <select name="Number of Guests" id="guests">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <ValidationError prefix="guests" field="guests" errors={state.errors} />
-        </select>
-        <br />
-        <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" name="Email" />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <br />
-        <textarea id="message" name="Message" />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-        <button type="submit" disabled={state.submitting}>
-          Submit
-        </button>
-      </form>
-      {/* <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicName">
-          <Form.Label type="text" name="name" htmlFor="name">
-            Name of Guest(s)
-          </Form.Label>
-          <Form.Control type="text" placeholder="Guest Full Name" />
-        </Form.Group>
-        <Form.Group controlId="exampleForm.ControlSelect1">
-          <Form.Label>Number of Guest(s)</Form.Label>
-          <Form.Control as="select">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label htmlFor="email">Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3} />
-        </Form.Group>
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-        <Button variant="primary" type="submit" disabled={state.submitting}>
-          Submit
-        </Button>
-      </Form> */}
+    <div className="rsvp" id="rsvp">
+      <div className="rsvp__container">
+        <h1>RSVP</h1>
+        <form className="rsvp__form" onSubmit={handleSubmit}>
+          <label className="rsvp__label">
+            Name of Guest(s):
+            <input type="text" name="Guest(s) Name" required />
+          </label>
+
+          <label className="rsvp__label" type="number" name="number" required>
+            Number of Guest(s):
+            <select name="Number of Guests" id="guests">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <ValidationError
+                prefix="guests"
+                field="guests"
+                errors={state.errors}
+              />
+            </select>
+          </label>
+
+          <label className="rsvp__label" htmlFor="email">
+            Email Address
+            <input id="email" type="email" name="Email" />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+          </label>
+
+          <label className="rsvp__label" htmlFor="message">
+            Message
+            <textarea id="message" name="Message" />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+          </label>
+
+          <button className='rsvp__button' type="submit" disabled={state.submitting}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
