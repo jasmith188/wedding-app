d5fa8d4bfeca80342c21c30ea570d0ef5d8bc4d6
James Smith <jasmith.188@gmail.com>
2021-04-30 21:34:15 -0400

    css master

--- src/components/ThingsToDo.js
+++ src/components/ThingsToDo.js
@@ -17,8 +17,10 @@ const ThingsToDo = () => {
 
         <h1>BILTMORE</h1>
         <br />
-        <h3>1 LODGE ST, ASHEVILLE, NC 28803, USA (800) 411-3812</h3>
-        <br />
+        <h3>
+          1 LODGE ST, ASHEVILLE, NC 28803, USA{' '}
+          <a href="tel:800-411-3812">(800) 411-3812</a>
+        </h3>
         <p>
           Biltmore Estate in Asheville, N.C. George Vanderbilt's 250-room castle
           is America's Largest Home and a top attraction in Asheville. Plan a
@@ -39,16 +41,15 @@ const ThingsToDo = () => {
         />
 
         <h1>HIGHLAND BREWING COMPANY</h1>
-        <br/>
+        <br />
         <h3>
           12 OLD CHARLOTTE HWY #200, ASHEVILLE, NC 28803, USA (828) 299-3370
         </h3>
-        <br/>
         <p>
           One of Jimmy's favorite beers is brewed here! - Cold Mountain Winter
-          Ale (seasonal) - Also try Gaelic Ale
+          Ale (seasonal) - Also try their Gaelic Ale!
         </p>
-        <br/>
+        <br />
         <a href="https://highlandbrewing.com/">
           <button>Link to Website</button>
         </a>
--- src/components/WeddingParty.js
+++ src/components/WeddingParty.js
@@ -6,169 +6,194 @@ import sirayaPic from '../weddingImages/siraya.jpg';
 import baileyPic from '../weddingImages/bailey.jpg';
 import reneePic from '../weddingImages/renee.jpg';
 import bridgetPic from '../weddingImages/bridget.jpg';
-import { Accordion, Card, Button } from 'react-bootstrap';
 
 const WeddingParty = () => {
   return (
     <div className="weddingParty" id="wedding">
       <h1>Wedding Party</h1>
-      <br />
-      <br />
-      <h2 className="weddingParty__title">Bride's Side</h2>
-      <Accordion>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              Click me!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>Hello! I'm the body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              Click me!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm another body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
-      <div className="weddingParty__card">
-        <img src={jenPic} alt="girl emoji" className="weddingParty__image" />
-        <h1 className="weddingParty__name">Jennifer Garrett - Maid of Honor</h1>
-        <br />
-        <p>
-          Jennifer and Katie met in Chicago while working at Mars in 2017. As
-          her best friend, it was only fitting to have Jennifer stand up next to
-          Katie as her Maid of Honor.
-        </p>
+      <div className="weddingParty__brides">
+        <h2 className="weddingParty__title">Bride's Side</h2>
+
+        <div className="row">
+          <div className="column">
+            <div className="weddingParty__card">
+              <img
+                src={jenPic}
+                alt="girl emoji"
+                className="weddingParty__image"
+              />
+              <h1 className="weddingParty__name">
+                Jennifer Garrett - Maid of Honor
+              </h1>
+
+              <p>
+                Jennifer and Katie met in Chicago while working at Mars in 2017.
+                As her best friend, it was only fitting to have Jennifer stand
+                up next to Katie as her Maid of Honor.
+              </p>
+            </div>
+          </div>
+
+          <div className="column">
+            <div className="weddingParty__card">
+              <img
+                src={sirayaPic}
+                alt="girl emoji"
+                className="weddingParty__image"
+              />
+              <h1 className="weddingParty__name">
+                Siraya Schleis - Bridesmaid
+              </h1>
+
+              <p>
+                Siraya is the sister of the bride and Katie wanted nothing more
+                than to have her by her side when she marries Jimmy. Siraya
+                worked on the cruise ship with Jimmy and Katie – so she has been
+                there since the very beginning!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <div className="weddingParty__card">
+              <img
+                src={lizPic}
+                alt="girl emoji"
+                className="weddingParty__image"
+              />
+              <h1 className="weddingParty__name">
+                Liz Jasurda-Love – Bridesmaid
+              </h1>
+              <br />
+              <p>
+                Liz (a kindred heart from Wisconsin) met Katie and Jimmy while
+                working on the cruise ship in Hawai’i in 2013. Having her with
+                us to share the day was an easy choice.
+              </p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="weddingParty__card">
+              <img
+                src={baileyPic}
+                alt="girl emoji"
+                className="weddingParty__image"
+              />
+              <h1 className="weddingParty__name">Bailey Smith – Bridesmaid</h1>
+              <br />
+              <p>
+                Bailey and Katie met in Chicago in 2019, only a couple months
+                before Katie and Jimmy moved to Florida. She quickly became a
+                close friend (and cherished cousin 😉) and there was no question
+                to have her a part of the day.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <div className="weddingParty__card">
+              <img
+                src={reneePic}
+                alt="girl emoji"
+                className="weddingParty__image"
+              />
+              <h1 className="weddingParty__name">Renee DuBois – Attendant</h1>
+              <br />
+              <p>
+                Renee and Katie met in Florida after matching on the Bumble BFF
+                app in 2019! Their ‘first date’ was a success and they have been
+                awesome friends ever since.
+              </p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="weddingParty__card">
+              <img
+                src={bridgetPic}
+                alt="girl emoji"
+                className="weddingParty__image"
+              />
+              <h1 className="weddingParty__name">
+                Bridget Crawford – Attendant
+              </h1>
+              <br />
+              <p>
+                Bridget and Katie met in Florida via the Bumble BFF app in 2019
+                – their ‘first date’ was a wine walk which solidified their
+                wonderful friendship!
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <br />
-      <div className="weddingParty__card">
-        <img src={sirayaPic} alt="girl emoji" className="weddingParty__image" />
-        <h1 className="weddingParty__name">Siraya Schleis - Bridesmaid</h1>
+      <div className="weddingParty__grooms">
+        <h1>Groom's Side</h1>
+        <div className="weddingParty__card">
+          <img
+            src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/socialmedia/apple/271/boy_light-skin-tone_1f466-1f3fb_1f3fb.png"
+            alt="girl emoji"
+            className="weddingParty__image"
+          />
+          <h1 className="weddingParty__name">Justin Merod</h1>
+          <br />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+            maxime ut, eos in corrupti voluptas nostrum tempora obcaecati
+            accusantium quo! Dolorem dolorum distinctio et temporibus corporis?
+            Earum debitis ad animi.
+          </p>
+        </div>
         <br />
-        <p>
-          Siraya is the sister of the bride and Katie wanted nothing more than
-          to have her by her side when she marries Jimmy. Siraya worked on the
-          cruise ship with Jimmy and Katie – so she has been there since the
-          very beginning!
-        </p>
-      </div>
-      <br />
-      <div className="weddingParty__card">
-        <img src={lizPic} alt="girl emoji" className="weddingParty__image" />
-        <h1 className="weddingParty__name">Liz Jasurda-Love – Bridesmaid</h1>
+        <div className="weddingParty__card">
+          <img
+            src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/socialmedia/apple/271/boy_light-skin-tone_1f466-1f3fb_1f3fb.png"
+            alt="girl emoji"
+            className="weddingParty__image"
+          />
+          <h1 className="weddingParty__name">Mike Dubois</h1>
+          <br />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+            maxime ut, eos in corrupti voluptas nostrum tempora obcaecati
+            accusantium quo! Dolorem dolorum distinctio et temporibus corporis?
+            Earum debitis ad animi.
+          </p>
+        </div>
         <br />
-        <p>
-          Liz (a kindred heart from Wisconsin) met Katie and Jimmy while working
-          on the cruise ship in Hawai’i in 2013. Having her with us to share the
-          day was an easy choice.
-        </p>
-      </div>
-      <br />
-      <div className="weddingParty__card">
-        <img src={baileyPic} alt="girl emoji" className="weddingParty__image" />
-        <h1 className="weddingParty__name">Bailey Smith – Bridesmaid</h1>
+        <div className="weddingParty__card">
+          <img
+            src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/socialmedia/apple/271/boy_light-skin-tone_1f466-1f3fb_1f3fb.png"
+            alt="girl emoji"
+            className="weddingParty__image"
+          />
+          <h1 className="weddingParty__name">Gabe Schuman</h1>
+          <br />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+            maxime ut, eos in corrupti voluptas nostrum tempora obcaecati
+            accusantium quo! Dolorem dolorum distinctio et temporibus corporis?
+            Earum debitis ad animi.
+          </p>
+        </div>
         <br />
-        <p>
-          Bailey and Katie met in Chicago in 2019, only a couple months before
-          Katie and Jimmy moved to Florida. She quickly became a close friend
-          (and cherished cousin 😉) and there was no question to have her a part
-          of the day.
-        </p>
-      </div>
-      <div className="weddingParty__card">
-        <img src={reneePic} alt="girl emoji" className="weddingParty__image" />
-        <h1 className="weddingParty__name">Renee DuBois – Attendant</h1>
-        <br />
-        <p>
-          Renee and Katie met in Florida after matching on the Bumble BFF app in
-          2019! Their ‘first date’ was a success and they have been awesome
-          friends ever since.
-        </p>
-      </div>
-      <div className="weddingParty__card">
-        <img
-          src={bridgetPic}
-          alt="girl emoji"
-          className="weddingParty__image"
-        />
-        <h1 className="weddingParty__name">Bridget Crawford – Attendant</h1>
-        <br />
-        <p>
-          Bridget and Katie met in Florida via the Bumble BFF app in 2019 –
-          their ‘first date’ was a wine walk which solidified their wonderful
-          friendship!
-        </p>
-      </div>
-      <h1>Groom's Side</h1>
-      <div className="weddingParty__card">
-        <img
-          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/socialmedia/apple/271/boy_light-skin-tone_1f466-1f3fb_1f3fb.png"
-          alt="girl emoji"
-          className="weddingParty__image"
-        />
-        <h1 className="weddingParty__name">Justin Merod</h1>
-        <br />
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-          maxime ut, eos in corrupti voluptas nostrum tempora obcaecati
-          accusantium quo! Dolorem dolorum distinctio et temporibus corporis?
-          Earum debitis ad animi.
-        </p>
-      </div>
-      <br />
-      <div className="weddingParty__card">
-        <img
-          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/socialmedia/apple/271/boy_light-skin-tone_1f466-1f3fb_1f3fb.png"
-          alt="girl emoji"
-          className="weddingParty__image"
-        />
-        <h1 className="weddingParty__name">Mike Dubois</h1>
-        <br />
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-          maxime ut, eos in corrupti voluptas nostrum tempora obcaecati
-          accusantium quo! Dolorem dolorum distinctio et temporibus corporis?
-          Earum debitis ad animi.
-        </p>
-      </div>
-      <br />
-      <div className="weddingParty__card">
-        <img
-          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/socialmedia/apple/271/boy_light-skin-tone_1f466-1f3fb_1f3fb.png"
-          alt="girl emoji"
-          className="weddingParty__image"
-        />
-        <h1 className="weddingParty__name">Gabe Schuman</h1>
-        <br />
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-          maxime ut, eos in corrupti voluptas nostrum tempora obcaecati
-          accusantium quo! Dolorem dolorum distinctio et temporibus corporis?
-          Earum debitis ad animi.
-        </p>
-      </div>
-      <br />
-      <div className="weddingParty__card">
-        <img
-          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/socialmedia/apple/271/boy_light-skin-tone_1f466-1f3fb_1f3fb.png"
-          alt="girl emoji"
-          className="weddingParty__image"
-        />
-        <h1 className="weddingParty__name">Nick Schubert</h1>
-        <br />
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-          maxime ut, eos in corrupti voluptas nostrum tempora obcaecati
-          accusantium quo! Dolorem dolorum distinctio et temporibus corporis?
-          Earum debitis ad animi.
-        </p>
+        <div className="weddingParty__card">
+          <img
+            src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/socialmedia/apple/271/boy_light-skin-tone_1f466-1f3fb_1f3fb.png"
+            alt="girl emoji"
+            className="weddingParty__image"
+          />
+          <h1 className="weddingParty__name">Nick Schubert</h1>
+          <br />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+            maxime ut, eos in corrupti voluptas nostrum tempora obcaecati
+            accusantium quo! Dolorem dolorum distinctio et temporibus corporis?
+            Earum debitis ad animi.
+          </p>
+        </div>
       </div>
     </div>
   );
