b854d6300d914ede7ad192d1565bbfda68a93343
James Smith <jamessmith@Jamess-MBP.attlocal.net>
2021-06-24 01:34:03 -0400

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,8 @@ import kandjicon from "../images/kandj.png";
 const Footer = () => {
   return (
     <div className="footer">
-      <img className="footer__icon" src={kandjicon} alt="kandj-icon" />
-      <hr />
       <p className="footer__date">11.6.2021</p>
+      <img className="footer__icon" src={kandjicon} alt="kandj-icon" />
       <p>
         &copy; 2021 created by <a href="https://www.jamessmithweb.com">Jimmy</a>
       </p>
