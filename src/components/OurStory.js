a2c6a04241d3896a5ea45548f34a8bdd007f77f7
jasmith188 <jasmith.188@gmail.com>
2021-06-25 12:03:45 -0400

    fix carousel pgotos

--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import Image1 from '../weddingImages/couple-smile.jpeg';
-import Image2 from '../weddingImages/30bday.jpeg';
+import React from "react";
+import Image1 from "../weddingImages/couple-smile.jpeg";
+import Image2 from "../weddingImages/30bday.jpeg";
 // import Image3 from '../weddingImages/beachpic.jpeg';
-import Image4 from '../weddingImages/boatpic.jpeg';
+import Image4 from "../weddingImages/boatpic.jpeg";
 // import Image5 from '../weddingImages/cubshat.jpeg';
-import Image6 from '../weddingImages/coldpic.jpeg';
-import Image7 from '../weddingImages/formalpic.jpeg';
-import Image8 from '../weddingImages/formalpic2.jpeg';
-import Image9 from '../weddingImages/newglarus.jpeg';
-import Image10 from '../weddingImages/packergame.jpeg';
-import Image11 from '../weddingImages/patiopic.jpeg';
-import Image12 from '../weddingImages/ringpic.jpeg';
-import Image13 from '../weddingImages/stpattys.jpeg';
-import Image14 from '../weddingImages/weddingpic.jpeg';
-import './OurStory.css';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import Image6 from "../weddingImages/coldpic.jpeg";
+import Image7 from "../weddingImages/formalpic.jpeg";
+import Image8 from "../weddingImages/formalpic2.jpeg";
+import Image9 from "../weddingImages/newglarus.jpeg";
+import Image10 from "../weddingImages/packergame.jpeg";
+import Image11 from "../weddingImages/patiopic.jpeg";
+import Image12 from "../weddingImages/ringpic.jpeg";
+import Image13 from "../weddingImages/stpattys.jpeg";
+import Image14 from "../weddingImages/weddingpic.jpeg";
+import Image15 from "../images/kandj-brewers-game.JPG";
+
+import "./OurStory.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const OurStory = () => {
   return (
@@ -29,17 +31,50 @@ const OurStory = () => {
         big island of O'ahu - it was forever after that.
       </p>
       <Carousel className="ourStory__carousel">
-        <div className="ourStory__carousel-slider">
-          <img
-            className="ourStory__image"
-            src={Image10}
-            alt="katieandjimmypic"
-          />
-        </div>
         <div>
           <img
             className="ourStory__image"
-            src={Image2}
+            src={Image15}
+            alt="katieandjimmypic"
+          />
+        </div>
+
+        <div>
+          <img
+            className="ourStory__image"
+            src={Image7}
+            alt="katieandjimmypic"
+          />
+        </div>
+
+        <div>
+          <img
+            className="ourStory__image"
+            src={Image9}
+            alt="katieandjimmypic"
+          />
+        </div>
+
+        <div>
+          <img
+            className="ourStory__image"
+            src={Image8}
+            alt="katieandjimmypic"
+          />
+        </div>
+
+        <div>
+          <img
+            className="ourStory__image"
+            src={Image6}
+            alt="katieandjimmypic"
+          />
+        </div>
+
+        <div>
+          <img
+            className="ourStory__image"
+            src={Image1}
             alt="katieandjimmypic"
           />
         </div>
@@ -55,38 +90,35 @@ const OurStory = () => {
         <div>
           <img
             className="ourStory__image"
-            src={Image6}
+            src={Image2}
             alt="katieandjimmypic"
           />
         </div>
+
         <div>
           <img
             className="ourStory__image"
-            src={Image7}
+            src={Image13}
             alt="katieandjimmypic"
           />
         </div>
+
+        <div className="ourStory__carousel-slider">
+          <img
+            className="ourStory__image"
+            src={Image10}
+            alt="katieandjimmypic"
+          />
+        </div>
+
         <div>
           <img
             className="ourStory__image"
-            src={Image8}
+            src={Image14}
             alt="katieandjimmypic"
           />
         </div>
-        <div>
-          <img
-            className="ourStory__image"
-            src={Image9}
-            alt="katieandjimmypic"
-          />
-        </div>
-        <div>
-          <img
-            className="ourStory__image"
-            src={Image1}
-            alt="katieandjimmypic"
-          />
-        </div>
+
         <div>
           <img
             className="ourStory__image"
@@ -98,20 +130,6 @@ const OurStory = () => {
           <img
             className="ourStory__image"
             src={Image12}
-            alt="katieandjimmypic"
-          />
-        </div>
-        <div>
-          <img
-            className="ourStory__image"
-            src={Image13}
-            alt="katieandjimmypic"
-          />
-        </div>
-        <div>
-          <img
-            className="ourStory__image"
-            src={Image14}
             alt="katieandjimmypic"
           />
         </div>
