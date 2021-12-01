2a9d1155226c86c87bba67d29b55bf6b63f11c9d
jasmith188 <jasmith.188@gmail.com>
2021-12-01 11:47:28 -0500

    fix issue

--- src/components/MainInfo.js
+++ src/components/MainInfo.js
@@ -1,64 +1,10 @@
 import React from 'react';
 import './MainInfo.css';
 import './RSVP.css';
-// import Countdown from 'react-countdown';
-// import { useForm, ValidationError } from '@formspree/react';
-// import axios from 'axios';
-
-// const url = `https://randomuser.me/api`;
 
 const MainInfo = () => {
-  // const [users, setUsers] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchUserData = async () => {
-  //     const resp = await fetch(url);
-  //     const users = await resp.json();
-  //     setUsers(users.results);
-  //     console.log(users.results);
-  //   };
-  //   fetchUserData();
-  // }, []);
-
-  // const renderer = ({ days, completed }) => {
-  //   if (completed) {
-  //     // Render a completed state
-  //     return 'Todays the big day!!!';
-  //   } else {
-  //     // Render a countdown
-  //     return (
-  //       <span className="mainInfo__countdown">
-  //         <strong>{days}</strong> days to go!
-  //         <br />
-  //       </span>
-  //     );
-  //   }
-  // };
-
   return (
     <div className="mainInfo" id="home">
-      {/* {users.map((user) => {
-        const {
-          gender,
-          name: { title, first, last },
-          location: {
-            street: { number, name },
-            city,
-            state,
-            country,
-            postcode,
-          },
-          email,
-          login: { uuid, username },
-        } = user;
-        return (
-          <div key={uuid}>
-            <h3>
-              {title}. {first} {last} & {title}. {first} {last}
-            </h3>
-          </div>
-        );
-      })} */}
       <div>
         <div className="mainInfo__nameTitle">L & R</div>
       </div>
